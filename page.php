62349ff9a36c6c44777a100bf2f2a0f707f6df0a
Amir.E Haji Hussaini <niamileo@gmail.com>
2020-08-27 15:28:42 +0430

    sub page mnue added

--- page.php
+++ page.php
@@ -26,15 +26,32 @@ if($theParent) { ?>
 }
 ?>
 
-  <!-- <div class="page-links">
-    <h2 class="page-links__title"><a href="#">About Us</a></h2>
-    <ul class="min-list">
-      <li class="current_page_item"><a href="#">Our History</a></li>
-      <li><a href="#">Our Goals</a></li>
-    </ul>
-  </div> -->
-
 <?php
+$testArray = get_pages(array(
+  'child_of' => get_the_ID()
+));
+
+if($theParent or $testArray) { ?>
+  <div class="page-links">
+    <h2 class="page-links__title"><a href="<?php echo get_the_permalink($theParent) ?>"><?php echo get_the_title($theParent); ?></a></h2>
+    <ul class="min-list">
+   <?php
+    if($theParent){
+      $findChildrenOf = $theParent;
+    } else{
+      $findChildrenOf = get_the_ID();
+    }
+
+    wp_list_pages(array(
+      'title_li' => NULL,
+      'child_of' => $findChildrenOf,
+      'sort_column' => 'menu_order'
+    ))
+    ?>
+    </ul>
+  </div>
+
+<?php }
   while (have_posts()) {
   the_post(); ?>
   <h2><?php the_title(); ?></h2>
